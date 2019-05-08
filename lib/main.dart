eedbcf0cfc131d41af57f7b2a75e20387a56275e
Shiwei Wang <772930508@qq.com>
2019-05-08 20:28:49 +0800

    v0.2.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:flutter_app/homePage.dart';
 
 void main() => runApp(new MyApp());
 
@@ -7,106 +7,112 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Startup Name Generator',
       theme: new ThemeData(
-        primaryColor: Colors.white,
+        primaryColor: Colors.blue,
       ),
-      home: new RandomWords(),
+      home: new ScaffoldRoute(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class ScaffoldRoute extends StatefulWidget {
   @override
-//  State<StatefulWidget> createState() {
-//    // TODO: implement createState
-//    return null;
-//  }
-  createState() => new RandomWordsState();
+  _ScaffoldRouteState createState() => _ScaffoldRouteState();
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final _saved = new Set<WordPair>();
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      new MaterialPageRoute(
-        builder: (context) {
-          final tiles = _saved.map(
-            (pair) {
-              return new ListTile(
-                title: new Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-          return new Scaffold(
-            appBar: new AppBar(
-              title: new Text('Saved Suggestions'),
-            ),
-            body: new ListView(children: divided),
-          );
-        },
-      ),
-    );
-  }
+class _ScaffoldRouteState extends State<ScaffoldRoute>
+    with SingleTickerProviderStateMixin {
+  int _selectedIndex = 0;
+  final barList = ['TADAY', '日常习惯', '更多'];
+  var _title = 'TADAY';
+  final _pageController = PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Startup Name Generator'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return new Divider();
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
+    final pageView = PageView(
+      controller: _pageController,
+      children: <Widget>[
+        new HomePage(),
+        new AllPage(),
+        new MorePage()
+      ],
+      onPageChanged: (index) {
+        _currentPageSatus(index);
       },
     );
+
+    return Scaffold(
+        appBar: AppBar(
+          //导航栏
+          title: Text(_title),
+          centerTitle: true,
+          actions: <Widget>[
+            //导航栏右侧菜单
+            IconButton(icon: Icon(Icons.share), onPressed: () {}),
+          ],
+        ),
+//      drawer: new MyDrawer(), //抽屉
+        bottomNavigationBar: BottomNavigationBar(
+          // 底部导航
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_today), title: Text(barList[0])),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.all_inclusive), title: Text(barList[1])),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.more_horiz), title: Text(barList[2])),
+          ],
+          currentIndex: _selectedIndex,
+          fixedColor: Colors.blue,
+          onTap: _onItemTapped,
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          //悬浮按钮
+            child: Icon(Icons.add),
+            onPressed: _onAdd),
+        body: pageView
+    );
+  }
+
+  void _onItemTapped(int index) {
+    _currentPageSatus(index);
+    _pageController.jumpToPage(index);
+  }
+  void _currentPageSatus(int index){
+    setState(() {
+      _selectedIndex = index;
+      _title = barList[index];
+    });
+  }
+
+  void _onAdd() {}
+}
+
+
+
+class AllPage extends StatefulWidget {
+  @override
+  _AllPageState createState() => _AllPageState();
+}
+
+class _AllPageState extends State<AllPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text('all');
+  }
+}
+
+class MorePage extends StatefulWidget {
+  @override
+  _MorePageState createState() => _MorePageState();
+}
+
+class _MorePageState extends State<MorePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text('more');
   }
 }
